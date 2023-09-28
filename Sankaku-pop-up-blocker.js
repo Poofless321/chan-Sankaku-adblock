504d69a1fcbf7ec9539f69e61e4171c1ab5814dd
Poofless321 <sanchesdarian@hotmail.com>
2023-09-28 12:29:27 -0400

    Update Sankaku-pop-up-blocker.js

--- Sankaku-pop-up-blocker.js
+++ Sankaku-pop-up-blocker.js
@@ -7,22 +7,38 @@
 // @match        https://chan.sankakucomplex.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=sankakucomplex.com
 // @grant        none
+// @run-at       document-start
 // ==/UserScript==
 
 (function() {
     'use strict';
+    let amount = 1
+    const targetNode = document.documentElement;
+    const config = { childList: true, subtree: true };
 
-let possibleModals = [...document.querySelectorAll('div')].filter(el => {
-    let children = el.children;
-    return children.length === 4 &&
-           children[0].tagName === 'STYLE' &&
-           children[1].tagName === 'DIV' &&
-           children[2].tagName === 'DIV' &&
-           children[3].tagName === 'SCRIPT';
-});
+    const callback = function(mutationsList, observer) {
 
-if (possibleModals.length) {
-    possibleModals[0].style.display = 'none';
-}
+        console.log("Initializing MutationObserver");
+        for(let mutation of mutationsList) {
+            if (mutation.type === 'childList') {
+                let possibleModals = [...document.querySelectorAll('div')].filter(el => {
+                    let children = el.children;
+                    return children.length === 4 &&
+                           children[0].tagName === 'STYLE' &&
+                           children[1].tagName === 'DIV' &&
+                           children[2].tagName === 'DIV' &&
+                           children[3].tagName === 'SCRIPT';
+                });
 
+                if (possibleModals.length) {
+                    console.log("Found a mutation with the shit you want");
+                    possibleModals[0].remove();
+                    observer.disconnect(); // disconnect observer after deleting the div
+                }
+            }
+        }
+    };
+
+    const observer = new MutationObserver(callback);
+    observer.observe(targetNode, config);
 })();
